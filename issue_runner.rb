8b71dbb79d015551df13dfa61adc8efaab79f925
Sully <a.m.sullivan.github@gmail.com>
2021-10-25 23:02:27 -0400

    Update in preparation for README

--- issue_runner.rb
+++ issue_runner.rb
@@ -7,24 +7,27 @@ require_relative './knowledge_check_issue_template'
 
 Dotenv.load
 
-# use a GitHub personal access token in the .env file, I have my token limited to repo and account info access only
 client = Octokit::Client.new(access_token: ENV.fetch('ACCESS_TOKEN'))
 
-# name of the course being referenced in the template
+# Edit the following four variables as applicable
 course_name = 'Foo'
-
-# names of the lessons being referenced in the template
 lesson_names = %w[
   bar
   bash
+  baz
+  bing
+  bang
+  bong
+  boom
+  bam
 ]
-
 related_to_issue_number = '#22222'
+owner_and_repo_name = 'account_name/repo_name' # eg 'wise-king-sullyman/TOP-automated-issue-creation'
 
 lesson_names.each do |lesson_name|
   issue_template = KnowledgeCheckIssueTemplate.new(course_name, lesson_name, related_to_issue_number)
 
-  client.create_issue('TheOdinProject/curriculum', issue_template.title, issue_template.body)
+  client.create_issue(owner_and_repo_name, issue_template.title, issue_template.body)
 
   # some amount of sleep is needed to avoid the secondary rate limit
   sleep 3
